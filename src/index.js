ab7457299e8f3e2f0d7ba696af561c4f3d9e777f
katheryn-k <kirichenko.ekaterynaa@yandex.ru>
2021-03-03 14:08:21 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+    n = Math.abs(n);
+    let numToString = n.toString().split('');
+    let reversedNum = numToString.reverse().join('');
+    return Number(reversedNum);
 }
